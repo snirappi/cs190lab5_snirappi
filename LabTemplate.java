d1df26a80f459c86787c35de4431f150ab73c869
Shawn Nirappil <snirappi@purdue.edu>
2013-11-13 11:48:23 -0500

    Lab5

---
+++ LabTemplate.java
@@ -0,0 +1,16 @@
+--2013-11-13 11:46:01--  https://github.com/scottopell/cs190lab5/raw/master/LabTemplate.java
+Resolving github.com... 192.30.252.131
+Connecting to github.com|192.30.252.131|:443... connected.
+HTTP request sent, awaiting response... 302 Found
+Location: https://raw.github.com/scottopell/cs190lab5/master/LabTemplate.java [following]
+--2013-11-13 11:46:01--  https://raw.github.com/scottopell/cs190lab5/master/LabTemplate.java
+Resolving raw.github.com... 199.27.72.133
+Connecting to raw.github.com|199.27.72.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 1552 (1.5K) [text/plain]
+Saving to: 'LabTemplate.java.1'
+
+     0K .                                                     100% 25.7M=0s
+
+2013-11-13 11:46:02 (25.7 MB/s) - 'LabTemplate.java.1' saved [1552/1552]
+
